5885e94b4eeb762390b333996465f9f61a1445ee
KkaiK <godmanm@163.com>
2018-09-15 17:25:27 +0800

    practice 'printf' and scanf'

--- 2.c
+++ 2.c
@@ -1,10 +1,33 @@
 #include<stdio.h>
+/*****************
+char text(char a)
+{
+	a = 10;
+	printf("A=%d\n",a);
+	return a;
+}
+
+
 
 int main()
 {
-	char a = 0x11;;
+	//char a = 0x11;;
 	//a  a;
-	printf("%x\n",a);
+	char b = 15;
+	char c;
+	c = text(b);
+	printf("B=%d\nC=%d\n",b,c);
 	return 0;
 
+}
+************************************/
+
+void main()
+{
+	printf("input A B C,at intervals of space.\n");
+	int A;int B;int C;
+	scanf("%d %d %d",&A,&B,&C);
+	printf("A is %d,B is %d,C is %d",A,B,C);
+	
+	
 }
